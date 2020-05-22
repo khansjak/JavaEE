f383d431c8556b5a2662fa488432c8b6cb98a633
JAK <khans.jak@gmail.com>
2020-05-22 23:36:48 +0530

    22 May 2020

--- SpringDemo/src/controller/SpringMain.java
+++ SpringDemo/src/controller/SpringMain.java
@@ -3,14 +3,21 @@ package controller;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.to.Employee;
 import com.to.Hello;
 
 public class SpringMain {
 
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-		Hello h = context.getBean(Hello.class);
-		System.out.println(h.getMessage());
+		Hello h1 = (Hello) context.getBean("h1");
+		Hello h2 = (Hello) context.getBean("h2");
+		Employee e1 = (Employee) context.getBean("e1");
+		
+		System.out.println(h1.getMessage());
+		System.out.println(h2.getMessage());
+		System.out.println(e1.getEmployeeId());
+		System.out.println(e1.getEmployeeName());
 	}
 
 }
